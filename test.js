34a4f895f0271543a204a3d9349823b8d3444ee0
sd12354 <78817767+sd12354@users.noreply.github.com>
2022-09-16 21:54:05 -0400

    website updates

--- test.js
+++ test.js
@@ -1,76 +1,49 @@
-console.log("this is working")
-class StickyNavigation {
-	
-	constructor() {
-		this.currentId = null;
-		this.currentTab = null;
-		this.tabContainerHeight = 70;
-		let self = this;
-		$('.et-hero-tab').click(function() { 
-			self.onTabClick(event, $(this)); 
-		});
-		$(window).scroll(() => { this.onScroll(); });
-		$(window).resize(() => { this.onResize(); });
-	}
-	
-	onTabClick(event, element) {
-		event.preventDefault();
-		let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
-		$('html, body').animate({ scrollTop: scrollTop }, 600);
-	}
-	
-	onScroll() {
-		this.checkTabContainerPosition();
-    this.findCurrentTabSelector();
-	}
-	
-	onResize() {
-		if(this.currentId) {
-			this.setSliderCss();
-		}
-	}
-	
-	checkTabContainerPosition() {
-		let offset = $('.et-hero-tabs').offset().top + $('.et-hero-tabs').height() - this.tabContainerHeight;
-		if($(window).scrollTop() > offset) {
-			$('.et-hero-tabs-container').addClass('et-hero-tabs-container--top');
-		} 
-		else {
-			$('.et-hero-tabs-container').removeClass('et-hero-tabs-container--top');
-		}
-	}
-	
-	findCurrentTabSelector(element) {
-		let newCurrentId;
-		let newCurrentTab;
-		let self = this;
-		$('.et-hero-tab').each(function() {
-			let id = $(this).attr('href');
-			let offsetTop = $(id).offset().top - self.tabContainerHeight;
-			let offsetBottom = $(id).offset().top + $(id).height() - self.tabContainerHeight;
-			if($(window).scrollTop() > offsetTop && $(window).scrollTop() < offsetBottom) {
-				newCurrentId = id;
-				newCurrentTab = $(this);
-			}
-		});
-		if(this.currentId != newCurrentId || this.currentId === null) {
-			this.currentId = newCurrentId;
-			this.currentTab = newCurrentTab;
-			this.setSliderCss();
-		}
-	}
-	
-	setSliderCss() {
-		let width = 0;
-		let left = 0;
-		if(this.currentTab) {
-			width = this.currentTab.css('width');
-			left = this.currentTab.offset().left;
-		}
-		$('.et-hero-tab-slider').css('width', width);
-		$('.et-hero-tab-slider').css('left', left);
-	}
-	
-}
+/* Please ❤ this if you like it! */
 
-new StickyNavigation();
+
+(function($) { "use strict";
+
+  $(function() {
+    var header = $(".start-style");
+    $(window).scroll(function() {    
+      var scroll = $(window).scrollTop();
+    
+      if (scroll >= 10) {
+        header.removeClass('start-style').addClass("scroll-on");
+      } else {
+        header.removeClass("scroll-on").addClass('start-style');
+      }
+    });
+  });   
+    
+  //Animation
+  
+  $(document).ready(function() {
+    $('body.hero-anime').removeClass('hero-anime');
+  });
+
+  //Menu On Hover
+    
+  $('body').on('mouseenter mouseleave','.nav-item',function(e){
+      if ($(window).width() > 750) {
+        var _d=$(e.target).closest('.nav-item');_d.addClass('show');
+        setTimeout(function(){
+        _d[_d.is(':hover')?'addClass':'removeClass']('show');
+        },1);
+      }
+  }); 
+  
+  //Switch light/dark
+  
+  $("#switch").on('click', function () {
+    if ($("body").hasClass("dark")) {
+      $("body").removeClass("dark");
+      $("#switch").removeClass("switched");
+    }
+    else {
+      $("body").addClass("dark");
+      $("#switch").addClass("switched");
+    }
+  });  
+  
+  })(jQuery);
